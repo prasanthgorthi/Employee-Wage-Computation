b1fcec4b1cfa1bc338dd3e6e3ef0cba73410bdfd
prasanthgorthi <117527406+prasanthgorthi@users.noreply.github.com>
2022-12-07 21:02:54 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,8 +1,25 @@
 class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Welcome Employee");
-    }
+{  
+      public static void Main(string[] args)
+        {
+          int isfulltime = 1;
+          int empRatePerHour = 20;
+
+          int empHrs = 1;
+          int empWage = 2;
+          Random random = new Random();
+          int empCheck = random.Next(0, 2);
+          if (empCheck == isfulltime)
+            {
+                empHrs = 8;
+
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
+    
 }
